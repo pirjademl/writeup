16d131989cfae54dc04e02b2b71f9a30aa1d6f12
Magdum <pirjademl7@gmail.com>
2025-06-27 16:03:10 +0530

    feat : add database persistence logic

---
+++ src/persistence/mysql.ts
@@ -0,0 +1,18 @@
+import mysql from 'mysql2/promise';
+import { createPool, Pool } from 'mysql2/promise';
+
+const pool: Pool = mysql.createPool({
+    host: process.env.MYSQL_PROTOCOL,
+    database: process.env.DATABASE_NAME,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+pool.on('connection', () => {
+    console.log('✅ MySQL pool connection established');
+});
+
+export { pool };
