4ed2f28f306eca06c3bf280fa36064a7d6ec1a6c
Magdum <pirjademl7@gmail.com>
2025-07-16 18:41:05 +0530

    feat: add debounce utility function to debounce any action

---
+++ src/app/lib/user.ts
@@ -0,0 +1,18 @@
+import { pool } from '@/persistence/mysql';
+import { User } from '../api/login/route';
+
+export async function getUserByEmail(username: String) {
+    try {
+        const [result] = await pool.execute<User[]>(
+            'SELECT firstName,lastName,email from users where email=?',
+            [username],
+        );
+        if (result.length === 0) {
+            return null;
+        }
+        const user = result[0];
+        return user;
+    } catch (err) {
+        console.log(err);
+    }
+}
